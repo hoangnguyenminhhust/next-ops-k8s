dc46174ac16ffdccf77a57ab5aa3bf8b0a2598ba
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-02-11 23:35:34 +0700

    hoang:

---
+++ bash.sh
@@ -0,0 +1,44 @@
+#!/bin/bash/#
+#install EKF
+cd EKF
+kubectl apply -f namespace.yml
+kubectl apply -f . 
+
+#install  jaeger
+cd ../jaeger/cassandra
+kubectl apply -f .
+
+cd ..
+kubectl apply -f . 
+
+#install prometheus
+
+cd ..
+cd prometheus
+cd install-prometheus
+kubectl create ns monitoring
+kubectl apply -f .
+#install grafana
+
+cd ..
+cd ..
+cd redis
+kubectl apply -f . 
+
+#master slave 
+#kubectl exec -it redis-cluster-0 -- redis-cli --cluster create $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
+
+#intsall ingress
+cd ingress
+kubectl apply -f .
+kubectl apply -f .
+kubectl apply -f .
+
+#install nats 
+
+cd ..
+cd nats
+cd nats.beta
+kubectl  apply -f all.nats.yaml
+
+kubectl apply -f .
--- cert/bash.sh
+++ cert/bash.sh
@@ -9,8 +9,8 @@ cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=rout
 cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=client client.json | cfssljson -bare client
 
 #create key success connections
-kubectl create secret generic nats-tls-example --from-file=ca.pem --from-file=server-key.pem --from-file=server.pem 
+kubectl create secret generic nats-tls --from-file=ca.pem --from-file=server-key.pem --from-file=server.pem 
 
-kubectl create secret generic nats-tls-routes-example --from-file=ca.pem --from-file=route-key.pem --from-file=route.pem 
+kubectl create secret generic nats-tls-routes --from-file=ca.pem --from-file=route-key.pem --from-file=route.pem 
 
-kubectl create secret generic nats-tls-client-example --from-file=ca.pem --from-file=client-key.pem --from-file=client.pem
+kubectl create secret generic nats-tls-client --from-file=ca.pem --from-file=client-key.pem --from-file=client.pem
