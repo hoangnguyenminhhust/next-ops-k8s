4980ca56a71024cb941bc0501ab8b67927fe1a4a
Le Van Tuong <tuonglv.bhsoft@gmail.com>
2020-02-12 16:06:47 +0700

    1

--- bash.sh
+++ bash.sh
@@ -3,52 +3,51 @@
 cd EKF
 kubectl apply -f namespace.yml
 kubectl apply -f . 
-echo -------------------
+cd ..
+echo ------------------------------------------------------------
 
 #install  jaeger
-cd ../jaeger/cassandra
+cd ./jaeger/cassandra
 kubectl apply -f .
-echo -------------------
-
 cd ..
 kubectl apply -f . 
-echo -------------------
+cd ..
+echo ------------------------------------------------------------
 
 #install prometheus
-
-cd ..
-cd prometheus
-cd install-prometheus
+cd ./prometheus/install-prometheus
 kubectl create ns monitoring
 kubectl apply -f .
-
-echo -------------------
+cd ..
+cd ..
+echo ------------------------------------------------------------
 #install grafana
 
-cd ..
-cd ..
 cd redis
 kubectl apply -f . 
-
 #master slave 
 #kubectl exec -it redis-cluster-0 -- redis-cli --cluster create $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
-echo -------------------
+cd ..
+echo ------------------------------------------------------------
 
 #intsall ingress
-cd ..
-
 cd ingress
 kubectl apply -f .
 kubectl apply -f .
 kubectl apply -f .
-echo -------------------
+cd ..
+echo ------------------------------------------------------------
 
 #install nats 
-
-cd ..
 cd nats
 cd nats.beta
 kubectl  apply -f all.nats.yaml
-
+cd ..
+cd ..
+cd cert 
+bash bash.sh
+cd ..
+cd nats
+cd nats.beta
 kubectl apply -f .
-echo -------------------
+echo ------------------------------------------------------------
